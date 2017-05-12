239dc82f24c67f5bca9f0c9a13d3353f0f261836
activeliang <activeliang@gmail.com>
2017-05-12 11:48:25 +0800

    j

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 
   validates :category_id, presence: { message: "分类不能为空" }
   validates :title, presence: { message: "名称不能为空" }
-  validates :status, inclusion: { in: %w[on off], 
+  validates :status, inclusion: { in: %w[on off],
     message: "商品状态必须为on | off" }
   validates :amount, numericality: { only_integer: true,
     message: "库存必须为整数" },
@@ -25,6 +25,39 @@ class Product < ApplicationRecord
   before_create :set_default_attrs
 
   scope :onshelf, -> { where(status: Status::On) }
+
+  module RandomCode
+    class << self
+      def generate_password len = 8
+        seed = (0..9).to_a + ('a'..'z').to_a + ('A'..'Z').to_a + ['!', '@', '#', '$', '%', '.', '*'] * 4
+        token = ""
+        len.times { |t| token << seed.sample.to_s }
+        token
+      end
+
+      def generate_cellphone_token len = 6
+        a = lambda { (0..9).to_a.sample }
+        token = ""
+        len.times { |t| token << a.call.to_s }
+        token
+      end
+
+      def generate_utoken len = 8
+        a = lambda { rand(36).to_s(36) }
+        token = ""
+        len.times { |t| token << a.call.to_s }
+        token
+      end
+
+      def generate_product_uuid
+        Date.today.to_s.split('-')[1..-1].join() << generate_utoken(6).upcase
+      end
+
+      def generate_order_uuid
+        Date.today.to_s.split('-').join()[2..-1] << generate_utoken(8).upcase
+      end
+    end
+  end
 
   module Status
     On = 'on'
