2ae64e7152788f8dad871f52eb8b8b10f08ca559
activeliang <activeliang@gmail.com>
2017-05-12 11:58:19 +0800

    J

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170216073235) do
 
-  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "address_type"
     t.string   "contact_name"
@@ -21,21 +21,21 @@ ActiveRecord::Schema.define(version: 20170216073235) do
     t.string   "zipcode"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["user_id", "address_type"], name: "index_addresses_on_user_id_and_address_type", using: :btree
+    t.index ["user_id", "address_type"], name: "index_addresses_on_user_id_and_address_type"
   end
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "categories", force: :cascade do |t|
     t.string   "title"
     t.integer  "weight",           default: 0
     t.integer  "products_counter", default: 0
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.string   "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
-    t.index ["title"], name: "index_categories_on_title", using: :btree
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+    t.index ["title"], name: "index_categories_on_title"
   end
 
-  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "product_id"
     t.integer  "address_id"
@@ -47,27 +47,27 @@ ActiveRecord::Schema.define(version: 20170216073235) do
     t.datetime "updated_at",                                               null: false
     t.integer  "payment_id"
     t.string   "status",                               default: "initial"
-    t.index ["order_no"], name: "index_orders_on_order_no", unique: true, using: :btree
-    t.index ["payment_id"], name: "index_orders_on_payment_id", using: :btree
-    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
+    t.index ["order_no"], name: "index_orders_on_order_no", unique: true
+    t.index ["payment_id"], name: "index_orders_on_payment_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "payments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "payments", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "payment_no"
     t.string   "transaction_no"
-    t.string   "status",                                                default: "initial"
-    t.decimal  "total_money",                  precision: 10, scale: 2
+    t.string   "status",                                  default: "initial"
+    t.decimal  "total_money",    precision: 10, scale: 2
     t.datetime "payment_at"
-    t.text     "raw_response",   limit: 65535
-    t.datetime "created_at",                                                                null: false
-    t.datetime "updated_at",                                                                null: false
-    t.index ["payment_no"], name: "index_payments_on_payment_no", unique: true, using: :btree
-    t.index ["transaction_no"], name: "index_payments_on_transaction_no", using: :btree
-    t.index ["user_id"], name: "index_payments_on_user_id", using: :btree
+    t.text     "raw_response"
+    t.datetime "created_at",                                                  null: false
+    t.datetime "updated_at",                                                  null: false
+    t.index ["payment_no"], name: "index_payments_on_payment_no", unique: true
+    t.index ["transaction_no"], name: "index_payments_on_transaction_no"
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
-  create_table "product_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "product_images", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "weight",             default: 0
     t.string   "image_file_name"
@@ -76,39 +76,39 @@ ActiveRecord::Schema.define(version: 20170216073235) do
     t.datetime "image_updated_at"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.index ["product_id", "weight"], name: "index_product_images_on_product_id_and_weight", using: :btree
-    t.index ["product_id"], name: "index_product_images_on_product_id", using: :btree
+    t.index ["product_id", "weight"], name: "index_product_images_on_product_id_and_weight"
+    t.index ["product_id"], name: "index_product_images_on_product_id"
   end
 
-  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "products", force: :cascade do |t|
     t.integer  "category_id"
     t.string   "title"
-    t.string   "status",                                             default: "off"
-    t.integer  "amount",                                             default: 0
+    t.string   "status",                               default: "off"
+    t.integer  "amount",                               default: 0
     t.string   "uuid"
-    t.decimal  "msrp",                      precision: 10, scale: 2
-    t.decimal  "price",                     precision: 10, scale: 2
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                                                         null: false
-    t.datetime "updated_at",                                                         null: false
-    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
-    t.index ["status", "category_id"], name: "index_products_on_status_and_category_id", using: :btree
-    t.index ["title"], name: "index_products_on_title", using: :btree
-    t.index ["uuid"], name: "index_products_on_uuid", unique: true, using: :btree
+    t.decimal  "msrp",        precision: 10, scale: 2
+    t.decimal  "price",       precision: 10, scale: 2
+    t.text     "description"
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["status", "category_id"], name: "index_products_on_status_and_category_id"
+    t.index ["title"], name: "index_products_on_title"
+    t.index ["uuid"], name: "index_products_on_uuid", unique: true
   end
 
-  create_table "shopping_carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shopping_carts", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "user_uuid"
     t.integer  "product_id"
     t.integer  "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_shopping_carts_on_user_id", using: :btree
-    t.index ["user_uuid"], name: "index_shopping_carts_on_user_uuid", using: :btree
+    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+    t.index ["user_uuid"], name: "index_shopping_carts_on_user_uuid"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
@@ -126,21 +126,21 @@ ActiveRecord::Schema.define(version: 20170216073235) do
     t.integer  "default_address_id"
     t.string   "cellphone"
     t.boolean  "is_admin",                        default: false
-    t.index ["activation_token"], name: "index_users_on_activation_token", using: :btree
-    t.index ["cellphone"], name: "index_users_on_cellphone", using: :btree
-    t.index ["email"], name: "index_users_on_email", using: :btree
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
-    t.index ["uuid"], name: "index_users_on_uuid", unique: true, using: :btree
+    t.index ["activation_token"], name: "index_users_on_activation_token"
+    t.index ["cellphone"], name: "index_users_on_cellphone"
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["uuid"], name: "index_users_on_uuid", unique: true
   end
 
-  create_table "verify_tokens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "verify_tokens", force: :cascade do |t|
     t.string   "token"
     t.string   "cellphone"
     t.datetime "expired_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cellphone", "token"], name: "index_verify_tokens_on_cellphone_and_token", using: :btree
+    t.index ["cellphone", "token"], name: "index_verify_tokens_on_cellphone_and_token"
   end
 
 end
